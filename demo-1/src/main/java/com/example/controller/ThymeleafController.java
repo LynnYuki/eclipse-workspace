407be1a6b777fb0259bf9237ed1db66a4980ad4f
lynnyuki <lynnyuki9527@163.com>
2019-01-04 15:36:22 +0800

    thymeleaf tab

--- demo-1/src/main/java/com/example/controller/ThymeleafController.java
+++ demo-1/src/main/java/com/example/controller/ThymeleafController.java
@@ -1,8 +1,15 @@
 package com.example.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.pojo.User;
 
 @Controller
 @RequestMapping("th")
@@ -17,5 +24,52 @@ public class ThymeleafController {
 	public String center() {
 		return "thymeleaf/center";
 	}
+	
+	@RequestMapping("test")
+	public String test(ModelMap map) {
+		User u =new User();
+		u.setName("superadmin");
+		u.setAge(18);
+		u.setPassword("123456");
+		u.setBirthday(new Date());
+		u.setDesc("<font color='red'><b>hello~~~~</b></font>");
+		map.addAttribute("user",u);
+		
+		User u1 = new User();
+		u1.setAge(19);
+		u1.setName("user");
+		u1.setPassword("123456");
+		u1.setBirthday(new Date());
+		
+		User u2 = new User();
+		u2.setAge(17);
+		u2.setName("admin");
+		u2.setPassword("123456");
+		u2.setBirthday(new Date());
+		
+		List<User> userList = new ArrayList<>();
+		userList.add(u);
+		userList.add(u1);
+		userList.add(u2);
+		
+		map.addAttribute("userList", userList);
+		
+        return "thymeleaf/test";
+		
+	}
+	@PostMapping("postform")
+	public String postform(User u) {
+		System.out.println("姓名"+u.getName());
+		System.out.println("年龄"+u.getAge());
+		return "redirect:/th/test";
+		
+	}
+	@RequestMapping("showerror")
+    public String showerror(User u) {
+		
+		int a = 1 / 0;
+		
+        return "redirect:/th/test";
+    }
 
 }
